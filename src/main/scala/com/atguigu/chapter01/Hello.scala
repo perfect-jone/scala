ae1e043813cafd1a269a0cc6c3034868601bf2a1
jone <jone@jone.com>
2020-03-24 16:03:50 +0800

    scala files

--- src/main/scala/com/atguigu/chapter01/Hello.scala
+++ src/main/scala/com/atguigu/chapter01/Hello.scala
@@ -10,7 +10,7 @@ package com.atguigu.chapter01
 * Ctrl+F12  查看当前类的所有方法
 * Ctrl+P  查看方法提示
 * Ctrl+Shift+Enter  自动补全
-* Crrl+W  选择当前光标位置单词并扩大
+* Ctrl+W  选择当前光标位置单词并扩大
 * Ctrl+Shift+W  选择当前光标位置单词并扩大
 * Shift+Shift 查找当前项目中的任何文件
 * Shit+Alt+↓ 当前行和下一行切换位置
