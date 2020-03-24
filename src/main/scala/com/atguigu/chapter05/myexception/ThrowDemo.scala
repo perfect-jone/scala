1e304577f91fbbda52ee507b34821a3211256c23
jone <jone@jone.com>
2020-03-24 17:14:15 +0800

    Merge branch 'master' of https://github.com/perfect-jone/scala
    # Conflicts:
    #	src/main/scala/com/atguigu/chapter01/Hello.scala

---
+++ src/main/scala/com/atguigu/chapter05/myexception/ThrowDemo.scala
@@ -0,0 +1,16 @@
+package com.atguigu.chapter05.myexception
+
+object ThrowDemo {
+  def main(args: Array[String]): Unit = {
+    try {
+      val res = test()
+      println(res.toString)
+    } catch {
+      case e => println("捕获到了异常"+ e.getMessage)
+    }
+  }
+
+  def test(): Nothing = {
+    throw new Exception("NO1异常")
+  }
+}
