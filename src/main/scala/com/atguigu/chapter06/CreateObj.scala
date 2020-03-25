751e7a0e5ac8bcb4ebf64dbf6e57d64d3d33b11f
jone <jone@jone.com>
2020-03-25 11:08:16 +0800

    commit

---
+++ src/main/scala/com/atguigu/chapter06/CreateObj.scala
@@ -0,0 +1,16 @@
+package com.atguigu.chapter06
+
+object CreateObj {
+  def main(args: Array[String]): Unit = {
+    val emp = new Emp //对象emp的类型是Emp
+    val emp2: Person = new Emp  //多态：子类给父类
+  }
+}
+
+class Person {
+
+}
+
+class Emp extends Person{
+
+}
