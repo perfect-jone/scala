b00e6272488984c6e9a42fce79f7d40a30cb9700
jone <jone@jone.com>
2020-04-10 11:43:45 +0800

    commit

--- src/main/scala/com/atguigu/chapter10/SetDemo01.scala
+++ src/main/scala/com/atguigu/chapter10/SetDemo01.scala
@@ -19,12 +19,12 @@ object SetDemo01 {
 
     val set3 = mutable.Set(1,3,6,9)//无序不重复
     println(set3)
-    println(set3.min+"\t"+set3.max+"\t"+set3.sum+"\t"+(set3.sum.toDouble/set3.size.toDouble))
-    println(set3.head+"\t"+set3.last+"\t"+set3.tail)
+    println(set3.min+"\t"+set3.max+"\t"+set3.sum+"\t"+(set3.sum.toDouble/set3.size.toDouble)) //Set集合的最大值、最小值、和、平均值
+    println(set3.head+"\t"+set3.last+"\t"+set3.tail) //头元素、尾元素、tail(去掉第一个元素的剩余集合)
 
     val list = mutable.ListBuffer(1,3,6,9,9)//有序重复
-    println(list.min+"\t"+list.max+"\t"+list.sum+"\t"+(list.sum.toDouble/list.size.toDouble))
-    println(list.head+"\t"+list.last+"\t"+list.tail)
+    println(list.min+"\t"+list.max+"\t"+list.sum+"\t"+(list.sum.toDouble/list.size.toDouble)) //ListBuffer集合的最大值、最小值、和、平均值
+    println(list.head+"\t"+list.last+"\t"+list.tail) //头元素、尾元素、tail(去掉第一个元素的剩余集合)
 
   }
 }
