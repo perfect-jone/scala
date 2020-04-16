a0e054780c618adf52bbc9e4444199fee8baa36c
jone <jone@jone.com>
2020-04-16 18:21:31 +0800

    commit

--- src/main/scala/com/atguigu/chapter13/HighOrderFunctionDemo02.scala
+++ src/main/scala/com/atguigu/chapter13/HighOrderFunctionDemo02.scala
@@ -9,8 +9,6 @@ object HighOrderFunctionDemo02 {
       (y: Int) => x - y
     }
     val res = minusxy(4)(3) //函数的柯里化
-    //比如有这么个需求：某个公司有200人，需要筛选出年龄在20到30之间的员工，（表名：emp；字段：id,name,age,sex,phoneNumber...),则就可以建立一个数据对象:
-    //select id,name,age,sex,phoneNumber from emp where age <= 30 and age >= 20
     println(res)
   }
 }
